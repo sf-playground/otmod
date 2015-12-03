c4490b7841d1bc92cbb1585b60a46999c861b205
Chris Simpkins <git.simpkins@gmail.com>
2015-12-03 16:05:49 -0500

    command line parsing class added, utf8 text file reader function added

--- otmod.py
+++ otmod.py
@@ -38,7 +38,7 @@ class Argument(list):
             if test_arg in self.argv:
                 return self.argv.index(test_arg)
             else:
-                return -1
+                return -1  ## TODO: change the return code that indicates an error
 
     # return the argument at the next position following a user specified positional argument (e.g. for argument to an option in cmd)
     def _getArgNext(self, position):
@@ -54,14 +54,14 @@ def read_utf8(filepath):
         import codecs
         f = codecs.open(filepath, encoding='utf_8', mode='r')
     except IOError as ioe:
-        sys.stderr.write("Unable to open '" + filepath + "' for read.")
+        sys.stderr.write("[otmod.py] ERROR: Unable to open '" + filepath + "' for read.\n")
         raise ioe
     try:
         textstring = f.read()
         norm_text = unicodedata.normalize('NFKD', textstring) # NKFD normalization of the unicode data before returns
         return norm_text
     except Exception as e:
-        sys.stderr.write("Unable to read the file with UTF-8 encoding using the read_utf8() method.")
+        sys.stderr.write("[otmod.py] ERROR: Unable to read " + filepath + " with UTF-8 encoding using the read_utf8() method.\n")
         raise e
     finally:
         f.close()
@@ -70,17 +70,23 @@ def read_utf8(filepath):
 def main(arguments):
     args = Argument(arguments)
 
-    # define command line syntax
+    # Define command line syntax
     if "--in" in args.argv:
-        print("in")
+        infile = args._getArgNext(args._getArgPosition("--in"))
+        if infile is "":
+            sys.stderr.write("[otmod.py] ERROR: please define the font input file path as an argument to the --in command line flag.\n")
+    elif "-i" in args.argv:
+        infile = args._getArgNext("--in")
+        if infile is "":
+            print("ERROR")
     else:
-        sys.stderr.write("[otmod.py] Please include the `--in` flag with an input font file defined as an argument")
+        sys.stderr.write("[otmod.py] Please include the `--in` flag with an input font file defined as an argument.\n")
         sys.exit(1)
 
     if "--opentype" in args.argv:
         print("opentype")
     else:
-        sys.stderr.write("[otmod.py] Please include the `--opentype` and define it with an path argument to the YAML formatted OpenType changes file")
+        sys.stderr.write("[otmod.py] Please include the `--opentype` and define it with an path argument to the YAML formatted OpenType changes file.\n")
         sys.exit(1)
 
     if "--out" in args.argv:
@@ -90,5 +96,5 @@ if __name__ == '__main__':
     if len(sys.argv) > 1:
         main(sys.argv[1:])
     else:
-        sys.stderr.write("[otmod.py] ERROR: no arguments detected in your command\n")
+        sys.stderr.write("[otmod.py] ERROR: no arguments detected in your command.\n")
         sys.exit(1)
